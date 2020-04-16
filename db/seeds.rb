84b6fdd1e05e72e839273ebcf5cdc8e6519ddfb3
EDEN <EDBEK@YAHOO.COM>
2020-04-16 18:00:09 -0500

    Added artist, event, and category info to seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+artists = Artist.create!(name: "Gertrude", description: "spoken word", bio: "lives in chicago", image: "", approved: true, highlighted: false)
+artists = Artist.create!(name: "Gretchen ", description: "unique earrings", bio: "lives in bay area", image: "", approved: true, highlighted: false)
+artists = Artist.create!(name: "Gabriela ", description: "acrylic paintings", bio: "lives in Indonesia", image: "", approved: true, highlighted: false)
+artists = Artist.create!(name: "Annie", description: "baking videos", bio: "lives in chicago", image: "", approved: true, highlighted: false)
+
+events = Event.create!(name: "farmer's market", date: "06/13/2020", location: "bay area", description: "support local vendors", artist_id: 2)
+events = Event.create!(name: "open mic", date: "06/13/2020", location: "Chicago", description: "view a variety of artists", artist_id: 4)
+
+categories = Category.create!(name: "food", image: "")
+categories = Category.create!(name: "jewelry", image: "")
+categories = Category.create!(name: "painting", image: "")
+categories = Category.create!(name: "words", image: "")
