40e2daa25d763b4fd2c43fb04a5cdd9da902df1a
EDEN <EDBEK@YAHOO.COM>
2020-04-28 16:09:33 -0500

    Added create action to events controller so that poeple can submit events for admin approval

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -8,4 +8,18 @@ class Api::EventsController < ApplicationController
     @event = Event.find_by(id: params[:id])
     render "show.json.jb"
   end
+
+  def create
+    @event = Event.new(
+      name: params[:name],
+      date: params[:date],
+      location: params[:location],
+      description: params[:description],
+    )
+    if @event.save
+      render "show.json.jb"
+    else
+      render json: { errors: @event.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
 
     get "/events" => "events#index"
     get "/events/:id" => "events#show"
+    post "/events" => "events#create"
   end
 end
